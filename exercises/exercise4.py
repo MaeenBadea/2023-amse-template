3881a29685f52f45140d310807d39053a46275f9
MaeenBadea <roronoamaeen@gmail.com>
2023-07-01 11:15:03 +0200

    fix columns

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -10,30 +10,14 @@ sql_types = {
     'Monat': types.BIGINT,
     'Temperatur': types.Float,
     'Batterietemperatur': types.Float,
-    'Batterietemperatur_1': types.Float,
     'Geraet aktiv': types.Text,
-    'Geraet aktiv_1': types.Text
-
-
 }
 
 def celsius_to_fahrenheit(c):
+    c = c.replace(',','.')
     c = float(c)
-
     return (c * 9/5) + 32
 
-def df_column_uniquify(df):
-    df_columns = df.columns
-    new_columns = []
-    for item in df_columns:
-        counter = 0
-        newitem = item
-        while newitem in new_columns:
-            counter += 1
-            newitem = "{}_{}".format(item, counter)
-        new_columns.append(newitem)
-    df.columns = new_columns
-    return df
 
 class Pipeline:
     zip_path = "./mowesta_data.zip"
@@ -49,17 +33,13 @@ class Pipeline:
 
     def extract_data(self):
         req.urlretrieve(self.dataset_path, Pipeline.zip_path)
-        # zf = zipfile.ZipFile(Pipeline.zip_path) 
-        # df = pd.read_csv(zf.open('data.csv'))
-        # df.describe()
-        if zipfile.is_zipfile(Pipeline.zip_path):
-            try:
-                zf = zipfile.ZipFile(Pipeline.zip_path) 
-                self.df = pd.read_csv(zf.open('data.csv'), sep=';', names = range(500))
-            except Exception as e:
-                print('error reading csv')
-        else:
-            print("File is not a zip file")
+
+        try:
+            zf = zipfile.ZipFile(Pipeline.zip_path) 
+            self.df = pd.read_csv(zf.open('data.csv'), sep=';', names = range(500))
+        except Exception as e:
+            print('error reading csv', e)
+
 
 
     def transform_data(self):
@@ -71,10 +51,9 @@ class Pipeline:
             "Batterietemperatur in °C" : "Batterietemperatur"
 
         })
-        self.df = df_column_uniquify(self.df)
-        self.df['Temperatur'] = self.df['Temperatur'].str.replace(',','.').map(celsius_to_fahrenheit)
-        self.df['Batterietemperatur'] = self.df['Batterietemperatur'].str.replace(',', '.').map(celsius_to_fahrenheit)
-        self.df['Batterietemperatur_1'] = self.df['Batterietemperatur_1'].str.replace(',', '.').map(celsius_to_fahrenheit)
+        self.df = self.df.iloc[:, ~self.df.columns.duplicated()]
+        self.df['Temperatur'] = self.df['Temperatur'].apply(celsius_to_fahrenheit)
+        self.df['Batterietemperatur'] = self.df['Batterietemperatur'].apply(celsius_to_fahrenheit)
         
 
     def load_data(self):
